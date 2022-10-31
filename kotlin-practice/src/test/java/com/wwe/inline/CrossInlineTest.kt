6be68620c15f1429d16080819822d8ba2d8fdeef
David B ZHANG <18588744489@163.com>
2022-10-31 09:17:06 +0800

    Update

--- kotlin-practice/src/test/java/com/wwe/inline/CrossInlineTest.kt
+++ kotlin-practice/src/test/java/com/wwe/inline/CrossInlineTest.kt
@@ -7,14 +7,31 @@ class CrossInlineTest {
 
     @Test
     fun test() {
-        hello {
-            return
+        println("doSth start")
+        doSthElse {
+            println("doSth else")
+            return // This is `non-local` returns
         }
+        println("doSth end")
     }
 
-    inline fun hello(postAction: () -> Unit) {
-        postAction()
+    private inline fun doSthElse(abc: () -> Unit) {
+        abc()
     }
 
+    /** This is how the `crossinline` can help us to avoid the "non-local returns". */
+    @Test
+    fun test_CrossInline() {
+        print("doSomething start")
+        doSthElse_CrossInline {
+            print("doSomethingElse")
+            // return // return is not allowed here
+        }
+        print("doSomething end")
+    }
 
+    /** We need to add the `crossinline`, then it will not allow us the put the return inside that lambdas like below: */
+    private inline fun doSthElse_CrossInline(crossinline abc: () -> Unit) {
+        abc()
+    }
 }

e49bb7d51afa7374857ffaba27a45a0e7145a1f3
David B ZHANG <18588744489@163.com>
2022-11-06 16:27:19 +0800

    Update

--- coroutine-best-practice/DAY6.md
+++ coroutine-best-practice/DAY6.md
@@ -1,3 +1,4 @@
+## Exceptions in coroutines
 
 In this article, we’ll explain how exceptions are propagated in coroutines and how you can always be in control, 
 including the different ways to handle them.
@@ -12,19 +13,57 @@ Then, the parent will
 2) cancel itself
 3) propagate the exception up to its parent.
 
+### SupervisorJob to the rescue
 
+With a `SupervisorJob`, the failure of a child does’t affect other children. 
+A `SupervisorJob` won’t cancel itself or the rest of its children. 
+Moreover, `SupervisorJob` won’t propagate the exception either, and will let the child coroutine handle it.
 
+You can create a `CoroutineScope` like this using `supervisorScope` or `CoroutineScope(SupervisorJob())`.
 
+```
+// In this case, if child#1 fails, neither scope nor child#2 will be cancelled.
+val scope = CoroutineScope(SupervisorJob())
+scope.launch {
+    // Child 1
+}
+scope.launch {
+    // Child 2
+}
+```
 
+Remember that a `SupervisorJob` only works as described when **it’s part of a scope**: 
+either created using `supervisorScope` or `CoroutineScope(SupervisorJob())`
 
-## Ref:
+### Async catch exception
 
-https://medium.com/androiddevelopers/easy-coroutines-in-android-viewmodelscope-25bffb605471
+**When `async` is used as a root coroutine, exceptions are thrown when you call `.await` .**
 
-https://medium.com/androiddevelopers/the-suspend-modifier-under-the-hood-b7ce46af624f
+```
+supervisorScope {
+    val deferred = async {
+        codeThatCanThrowExceptions()
+    }
+    try {
+        deferred.await()
+    } catch(e: Exception) {
+        // Handle exception thrown in async
+    }
+}
+```
 
-https://medium.com/androiddevelopers/suspending-over-views-19de9ebd7020
+### CoroutineExceptionHandler
 
-https://medium.com/androiddevelopers/coroutines-first-things-first-e6187bf3bb21
+The `CoroutineExceptionHandler` is an optional element of a `CoroutineContext` allowing you to handle uncaught exceptions.
 
-https://www.youtube.com/watch?v=w0kfnydnFWI
+```
+val handler = CoroutineExceptionHandler { context, exception ->
+    println("Caught $exception")
+}
+```
+
+**Exceptions will be caught if these requirements are met:**
+
+- When ⏰: The exception is thrown by a coroutine that automatically **throws exceptions** (works with `launch`, not with `async`).
+
+- Where 🌍: If it’s in the CoroutineContext of a CoroutineScope or a root coroutine (direct child of CoroutineScope or a supervisorScope).
---
+++ coroutine-best-practice/src/test/java/com/wwe/coroutine/DAY6Test.kt
@@ -0,0 +1,144 @@
+package com.wwe.coroutine
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import java.io.IOException
+
+/**
+ * Remember to use `SupervisorJob` when you want to avoid propagating cancellation when an exception happens, and Job otherwise.
+ * Uncaught exceptions will be propagated, catch them to provide a great UX!
+ */
+class DAY6Test {
+
+    @Test
+    fun test_supervisorScopeBehavior_badPractice() = runTest {
+        val scope = CoroutineScope(Job())
+        // SupervisorJob does nothing in this code!
+        scope.launch(SupervisorJob()) {
+            launch {
+                throw IOException("Bad net")
+            }
+            launch {
+                // child#2 will be cancelled.
+                println("#child2 done")
+            }
+            println("#parrent done")
+        }
+    }
+
+    @Test
+    fun test_supervisorScopeBehavior_goodPractice() = runTest {
+        val scope = CoroutineScope(Job())
+        scope.launch {
+            supervisorScope {
+                launch {
+                    throw IOException("Bad net")
+                }
+                launch {
+                    println("#child2 done")
+                }
+                println("#supervisorScope done")
+            }
+            println("#parrent done")
+        }
+    }
+
+    @Test
+    fun test_supervisorScopeBehavior_goodPractice2() = runTest {
+        val scope = CoroutineScope(SupervisorJob())
+        scope.launch {
+            throw IOException("Bad net")
+        }
+        scope.launch {
+            // child#2 will not be cancelled.
+            println("#child2 done")
+        }
+    }
+
+    @Test
+    fun test_lauch_handle_exception() = runTest {
+        coroutineScope {
+            try {
+                launch {
+                    /** With launch, exceptions will be thrown as soon as they happen */
+                    throw IOException("Bad net")
+                }
+            } catch (e: Exception) {
+                /**
+                 * using `coroutineScope`, Exception thrown in async WILL NOT be caught here
+                 * but propagated up to the scope
+                 */
+                println("WILL NOT catch Exception")
+            }
+        }
+    }
+
+    @Test
+    fun test_asynOperation_handle_exception() = runTest {
+        /**
+         *  Exceptions thrown in a `coroutineScope` builder or in coroutines created by other coroutines won’t be caught in a try/catch!
+         *  The reason is that will automatically propagate the exception up to its parent that will throw the exception.
+         */
+        coroutineScope {
+            val deferred = async {
+                throw IOException("Bad net")
+            }
+            try {
+                deferred.await()
+            } catch (e: Exception) {
+                /**
+                 * using `coroutineScope`, Exception thrown in async WILL NOT be caught here
+                 * but propagated up to the scope
+                 */
+                println("WILL NOT catch Exception")
+            }
+        }
+    }
+
+    @Test
+    fun test_supervisorScope_handle_exception() = runTest {
+        supervisorScope {
+            try {
+                val deferred = async {
+                    throw IOException("Bad net")
+                }
+                deferred.await()
+            } catch (e: Exception) {
+                println("WILL catch the Exception")
+            }
+        }
+    }
+
+    private val handler = CoroutineExceptionHandler { context, exception ->
+        println("Caught $exception")
+    }
+
+    @Test
+    fun test_CoroutineExceptionHandler_caughtException() = runTest {
+        val scope = CoroutineScope(Job())
+        // the exception will be caught by the handler:
+        scope.launch(handler) {
+            launch {
+                throw Exception("Failed coroutine")
+            }
+        }
+    }
+
+    @Test
+    fun test_CoroutineExceptionHandler_uncaughtException() = runTest {
+        val scope = CoroutineScope(Job())
+        // the exception will be caught by the handler:
+        scope.launch {
+            // the handler is installed in a inner coroutine, it won’t be caught exception
+            /**
+             * The inner launch will propagate the exception up to the parent as soon as it happens,
+             * since the parent does’t know anything about the handler,
+             * the exception will be thrown.
+             */
+            launch(handler) {
+                throw Exception("Failed coroutine")
+            }
+        }
+    }
+}

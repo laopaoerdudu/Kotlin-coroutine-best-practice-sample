903006ccf5e00440c6e4b6b4e9c3ecca641af5a9
David B ZHANG <18588744489@163.com>
2022-11-07 20:23:50 +0800

    Update

--- coroutine-best-practice/src/test/java/com/wwe/coroutine/DAY8Test.kt
+++ coroutine-best-practice/src/test/java/com/wwe/coroutine/DAY8Test.kt
@@ -6,11 +6,16 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
+import java.io.IOException
 
 class DAY8Test {
 
     @get:Rule
     var coroutinesTestRule = MainDispatcherRule()
+
+    private val handler = CoroutineExceptionHandler { _, exception ->
+        println("Caught $exception")
+    }
 
     @Test
     fun test() = runTest(coroutinesTestRule.testDispatcher) {
@@ -22,9 +27,14 @@ class DAY8Test {
     fun test_viewModelScope_case() = runTest(coroutinesTestRule.testDispatcher) {
         MainViewModel().sampleMethod()
     }
+
+    @Test
+    fun test_sampleMethod_withOtherCoroutineScope() = runTest(coroutinesTestRule.testDispatcher) {
+        MainViewModel().sampleMethod_withOtherCoroutineScope()
+    }
 }
 
-class MainViewModel : ViewModel() {
+class MainViewModel: ViewModel() {
 
     private val handler = CoroutineExceptionHandler { context, exception ->
         println("Caught $exception")
@@ -33,13 +43,29 @@ class MainViewModel : ViewModel() {
     fun sampleMethod() {
         viewModelScope.launch(handler) {
             launch {
+               // throw CancellationException()
+                //throw IOException("Bad net")
                 delay(500)
-                throw CancellationException()
-                println("job done")
+                println("child1 done")
             }.join()
 
             launch {
-                println("child done")
+                println("child2 done")
+            }
+
+            println("DONE")
+        }
+    }
+
+    fun sampleMethod_withOtherCoroutineScope() {
+        val scope = CoroutineScope(Job() + Dispatchers.Main + handler)
+        scope.launch {
+            launch {
+                throw IOException("Bad net")
+            }.join()
+
+            launch {
+                println("child2 done")
             }
         }
     }
